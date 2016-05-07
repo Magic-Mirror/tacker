752ab71579c24f12c36a82a2b6f450d77e156f72
Han Manjong <han@manjong.org>
2016-05-07 16:40:15 +0900

    Fix the error which is occurred by supporting multi vdus on creating SFC chain temporarliy

--- tacker/sfc/plugin.py
+++ tacker/sfc/plugin.py
@@ -73,7 +73,7 @@ class SFCPlugin(sfc_db.SFCPluginDb):
             sfc_dict[vnf]['ip'] = re.search(r'[0-9]+(?:\.[0-9]+){3}', vnf_data['mgmt_url']).group()
             # trozet check here to see how services are passed
             # we can only specify 1 atm for ODL
-            sfc_dict[vnf]['type'] = vnf_data['attributes']['service_type']
+            sfc_dict[vnf]['type'] = json.loads(vnf_data['attributes']['vdu1'])['service_type']
             sfc_dict[vnf]['name'] = vnf_data['name']
             # we also need the neutron port ID
             # tacker doesnt find this so we can use the vnf id to find the
